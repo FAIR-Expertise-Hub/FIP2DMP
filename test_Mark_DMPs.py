90b8635d4009d42aef232dcc2e29562919aa6be2
Nav Kaur <navkaur@Navs-MacBook-Air.local>
2022-12-27 14:10:11 +0100

    add comments to statistical analysis file

--- test_Mark_DMPs.py
+++ test_Mark_DMPs.py
@@ -1,23 +1,27 @@
-# general description about this code
+# This code takes the DMPs in the provided JSON files, prints out template information for each DMP (including RDF Triples).
 
 import json
 
 from collections import Counter
 
+# The id of each file contained in a list.
 dmp_file_ids = [102898, 110561, 64375,  83797,  94351,
 104396, 111527, 75184,  84265,  95832,
 105685, 38304,  77722,  85799,  96070,
 105776, 56773,  78027,  88134,  96543,
 106382, 64337,  78822,  90090,  98079]
 
-print('From the Open Science team, we obtained in total ', len (dmp_file_ids),  ' DMPs.')
+print ('From the Open Science team, we obtained in total ', len (dmp_file_ids),  ' DMPs.')
 
+# To keep count of each template in order to generate statistics.
 tmp_ct = Counter()
 
+# This code generates the URI which will be used for the RDF Triple.
 linked_dmp_namespace = 'https://linked-dmp.vu.nl/v0.1/'
 uses_template = linked_dmp_namespace+'uses_dmp_template'
 has_dmp_id = linked_dmp_namespace+'has_dmp_id'
 
+# Create dictionary for each DMP template and the URI.
 map_dmp_name_to_URI = {}
 map_dmp_name_to_URI ["Data Management Plan NWO (September 2020)"] = 'https://www.nwo.nl/dmp_template_2020'
 map_dmp_name_to_URI ["VU DMP template for research with personal data"] = 'https://templates.vu.nl/dmp_template'
@@ -29,7 +33,7 @@ map_dmp_name_to_URI ["VU DMP template 2021"] = 'https://templates.vu.nl/dmp_temp
 map_dmp_name_to_URI ["DCC Template"] = 'https://templates.vu.nl/dcc_template'
 
 path_to_files = './dmps/'
-# for each file, we take the template information
+# For each file, we take the template information.
 for file_id in dmp_file_ids:
     print ('file id = ', file_id)
     file_name = str(file_id) + '.json'
@@ -47,10 +51,9 @@ for file_id in dmp_file_ids:
         print ('----The RDF Triple----')
         print ('<'+ dmp_uri+ '> <'+ has_dmp_id+ '> "'+ str(file_id) + '"^^http://www.w3.org/2001/XMLSchema#integer.')
         print ('<'+ dmp_uri+ '> <'+ uses_template+ '> <'+ template_uri+ '>.')
-        print('\n\n')
+        print ('\n\n')
 
-# Next, we print some statistics of the template information
-
+# Next, we print some statistics of the template information.
 print('\nThe following is a summary of these templates:')
 for tmp_name in tmp_ct.keys():
     print ('template ', tmp_name, ' has ', tmp_ct[tmp_name])
